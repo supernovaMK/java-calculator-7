649bdb12ccea00553a48cba5234b3343f5631cbf
[supernovaMK] <[minki5336@naver.com]>
2024-10-18 01:00:08 +0900

    refactor(InputValidator) InputValidator 클래스 수정
    - 커스텀 구분자는 기본 구분자로 설정할 수 없기에 이를 수정하였습니다.
    - 커스텀 구분자는 숫자로 설저할 수 없기에 이를 수정하였습니다.

--- src/main/java/calculator/model/InputValidator.java
+++ src/main/java/calculator/model/InputValidator.java
@@ -23,8 +23,18 @@ public class InputValidator {
     private void checkCustomDelimiterValidate() {
         if (input.matches("^//.\\\\n.*")) {
 
+            checkCustomDelimiterRules(input.charAt(2));
             customDelimiter = input.charAt(2);
             bodyInput = input.substring(5);
+        }
+    }
+
+    private void checkCustomDelimiterRules(Character input) {
+        if (input == DelimiterInputData.COLON || input == DelimiterInputData.COMMA) {
+            throw new IllegalArgumentException("기본 연산자는 커스텀 연산자로 사용할 수 없습니다.");
+        }
+        if (input >= '0' && input < '9') {
+            throw new IllegalArgumentException("구분자의 문자는 숫자가 될 수 없습니다.");
         }
     }
 
