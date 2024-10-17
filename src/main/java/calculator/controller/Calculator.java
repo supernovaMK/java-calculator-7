a07a6945209c9a56c798494eed23e2ed48f756fa
[supernovaMK] <[minki5336@naver.com]>
2024-10-17 11:13:57 +0900

    refactor(Calculator): Calculator 클래스 수정
    - StringInput 클래스 수정에 맞춰 Calculator 컨트롤러 클래스를 수정하였습니다.

--- src/main/java/calculator/controller/Calculator.java
+++ src/main/java/calculator/controller/Calculator.java
@@ -1,24 +1,33 @@
 package calculator.controller;
 
-import calculator.Model.StringInput;
+import calculator.model.Calculate;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
 
     private final String STARTMESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private final String RESULTMESSAGE = "결과 : ";
+
+
+    private StringInput stringInput;
 
 
     public void start() {
         System.out.println(STARTMESSAGE);
-        StringInput input = readInput();
 
+        stringInput = readInput();
+
+        Calculate calculate = new Calculate(stringInput.numbers());
+
+        System.out.println(RESULTMESSAGE + calculate.sum());
     }
+
 
     private StringInput readInput() {
         String line = Console.readLine();
 
         return new StringInput(line);
-    }
 
+    }
 
 }
