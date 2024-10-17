c854c0e1160f2c9f35568b597b1a58ec0a5a9069
[supernovaMK] <[minki5336@naver.com]>
2024-10-17 11:22:55 +0900

    feat(SortNumbers): SortNumbers 클래스 구현
    - 사용자에게 입력 받은 문자열에서 원하는 데이터 추출 작업을 하기 위해,SortNumbers 클래스를 구현하였습니다.

---
+++ src/main/java/calculator/model/SortNumbers.java
@@ -0,0 +1,44 @@
+package calculator.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class SortNumbers {
+    private Character customDelimiter;
+    private String input;
+
+    public SortNumbers(DelimiterInputData delimiterInputData) {
+        this.customDelimiter = delimiterInputData.getcustomedDelimiter();
+        this.input = delimiterInputData.getInput();
+    }
+
+
+    public List<Long> findNumbers() {
+
+        if (input.isEmpty()) {
+            return null;
+        }
+
+        String reg = makeReqularExpression(customDelimiter);
+
+        return Arrays.stream(input.split(reg))
+                .filter(Objects::nonNull)
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+    }
+
+    private String makeReqularExpression(Character customDelimiter) {
+        return "[" + DelimiterInputData.COMMA + DelimiterInputData.COLON + changeExceptionWord(
+                String.valueOf(customDelimiter)) + "]";
+    }
+
+    private String changeExceptionWord(String str) {
+
+        return str.replace("\\", "\\\\")
+                .replace("[", "\\[")
+                .replace("]", "\\]");
+    }
+
+}
